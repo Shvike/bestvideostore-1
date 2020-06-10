a36484b136757983ea80bf4a6ee78cf6cc3d0cce
Bogdan <bogdan@mail.ru>
2020-06-10 21:32:16 +0300

    add main template and comment field

--- showvideo/views.py
+++ showvideo/views.py
@@ -14,19 +14,18 @@ def world(request):
 
 
 def accept_comment(request, id):
-    # 1
-    Comment.objects.create(text=request.GET["com"], comment_video_id=id)
-    # 2
-    # video = Video.objects.get(id=id)
-    # Comment.objects.create(text="hello world", comment_video=video)
-    # 3
-    # c = Comment(text=request.GET['com'], comment_video_id=id)
-    # c.save()
-    # print(id)
-    # print(request.GET["com"])
+    Comment.objects.create(text=request.POST["com"], comment_video_id=id)
+    print(request.POST["pwd"])
     return redirect("main_page")
 
 
 def one_video(request, id):
     response = {"video":Video.objects.get(id=id)}
     return render(request, "one_video.html", response)
+
+
+def add_like(request, id):
+    video = Video.objects.get(id=id)
+    video.likes += 2
+    video.save()
+    return redirect("main_page")
