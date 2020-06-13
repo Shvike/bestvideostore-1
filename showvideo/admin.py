5830d4ea8f0f01dd8dbe728f89eb7e00f015ae27
Bogdan <bogdan@mail.ru>
2020-06-13 12:39:33 +0300

    add js script and admin tools

--- showvideo/admin.py
+++ showvideo/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import Video, Comment
 
 
-admin.site.register(Video)
-admin.site.register(Comment)
+class CommentAdmin(admin.StackedInline):
+    model = Comment
+
+
+class VideoAdmin(admin.ModelAdmin):
+    inlines = [CommentAdmin]
+    readonly_fields = ["likes"]
+    list_display = ["title", "likes", "test", "tv"]
+    list_filter = ["likes", "date"]
+    prepopulated_fields = {"slug":["title"]}
+    search_fields = ["date"]
+    # filter_horizontal = ["date"]
+    # filter_vertical = ["date"]
+
+
+admin.site.register(Video, VideoAdmin)
+# admin.site.register(Comment)
 # Register your models here.
--- showvideo/models.py
+++ showvideo/models.py
@@ -1,15 +1,36 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 
 class Video(models.Model):       # A 1      B   2     C  3
-    urls = models.URLField()
-    title = models.CharField(max_length=150)
-    date = models.DateTimeField(auto_now_add=True)
-    description = models.TextField()
-    likes = models.PositiveIntegerField(default=0)
+    class Meta:
+        verbose_name = "Видео"
+        verbose_name_plural = "много видео"
+
+    slug = models.SlugField(
+        unique=True,
+        verbose_name="Слаг",
+        help_text="слаг должен быть уникальным"
+    )
+    urls = models.URLField(verbose_name="Урл, понял?")
+    title = models.CharField(max_length=150, verbose_name="название")
+    date = models.DateTimeField(auto_now_add=True, verbose_name="дата")
+    description = models.TextField(
+        verbose_name="описание",
+        null=True,
+        blank=True)
+    likes = models.PositiveIntegerField(default=0, verbose_name="нравится)")
 
     def __str__(self):
         return self.title
+
+    @property
+    def test(self):
+        return f"hello {self.title}{self.likes ** 2}"
+
+    @property
+    def tv(self):
+        return mark_safe(f"<iframe width='100' height='50' src='{self.urls}'></iframe>")
 # url_a, ..... 1
 # url_b, ..... 2
 # url_c, ..... 3
